f895706d04ecaacddff4067335cf931dd3e6497e
Ricardo Santos Dias <r247361@dac.unicamp.br>
2022-05-01 02:30:39 -0300

    Declare snake as linked list

--- structs.c
+++ structs.c
@@ -3,10 +3,12 @@
 typedef struct  body{
     int coordinates[2];
     body* next; //Pointer to the next body piece
+    body* previous;
 }body;
 
 typedef struct snake{
     int size;
     int direction[2];
-    body*body;
-}
+    body* head;
+    body* tail;
+}Snake;
